68cd9795471ee108d7f8e3118fb7ee86df598625
Veronika Darst <concitta@yandex.ru>
2024-08-12 13:59:15 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+example = 'Моя домашняя работа'
+print(example [0])
+print(example[-1])
+len(example)
+letter_count = len(example)
+print(letter_count)
+print(example[9:])
+print(example[::-1])
+print(example[::2])
